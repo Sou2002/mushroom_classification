8ba4b6311e38f7e6d8fc41aca4a3fde6ac4ceef9
Sourav Banerjee <souravbnjee07@gmail.com>
2024-04-24 20:35:12 +0530

    Added data transformation config in configuration.py

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -2,7 +2,8 @@
 
 from src.constants import *
 from src.utils.common import read_yaml, create_directories
-from src.entity.config_entity import (DataIngestionConfig)
+from src.entity.config_entity import (
+    DataIngestionConfig, DataTransformationConfig)
 
 
 class ConfigurationManager:
@@ -31,3 +32,15 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
