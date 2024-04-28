3dcc7c6dc757c86603b3d82bbacb9c1544854237
Ankita Banerjee <banerjeeankita031@gmail.com>
2024-04-28 12:09:44 +0530

    Updated index.py

--- index.py
+++ index.py
@@ -1,54 +1,72 @@
 import streamlit as st
+import requests
+
+st.set_page_config(layout="wide")
 
 st.title("Mushroom Classification")
 
 st.markdown("### Select Characteristics")
 
+my_dict = {}
+
 with st.form("form 1"):
     col1, col2, col3 = st.columns(3)
-    col1.selectbox('Cap Shape', options=(
+    my_dict['cap_shape'] = col1.selectbox('Cap Shape', options=(
         'Bell', 'Conical', 'Convex', 'Flat', 'Knobbed'), index=None, placeholder='Choose an option')
-    col2.selectbox('Cap Surface', options=(
+    my_dict['cap_surface'] = col2.selectbox('Cap Surface', options=(
         'Fibrous', 'Grooves', 'Scaly', 'Smooth'), index=None, placeholder='Choose an option')
-    col3.selectbox('Cap Color', options=('Brown', 'Buff', 'Cinnamon',
+    my_dict['cap_color'] = col3.selectbox('Cap Color', options=('Brown', 'Buff', 'Cinnamon',
                                          'Gray', 'Green', 'Pink', 'Purple', 'Red', 'White', 'Yellow'), index=None, placeholder='Choose an option')
-    col1.selectbox('Bruises', options=('Yes', 'No'),
+    my_dict['bruises'] = col1.selectbox('Bruises', options=('Yes', 'No'),
                    index=None, placeholder='Choose an option')
-    col2.selectbox('Odor', options=('Almond', 'Anise', 'Creosote',
+    my_dict['odor'] = col2.selectbox('Odor', options=('Almond', 'Anise', 'Creosote',
                                     'Fishy', 'Foul', 'Musty', 'None', 'Pungent', 'Spicy'), index=None, placeholder='Choose an option')
-    col3.selectbox('Gill Attachment', options=(
+    my_dict['gill_attachment'] = col3.selectbox('Gill Attachment', options=(
         'Attached', 'Descending', 'Free', 'Notched'), index=None, placeholder='Choose an option')
-    col1.selectbox('Gill Spacing', options=('Closed', 'Crowded',
+    my_dict['gill_apacing'] = col1.selectbox('Gill Spacing', options=('Closed', 'Crowded',
                    'Distant'), index=None, placeholder='Choose an option')
-    col2.selectbox('Gill Size', options=('Broad', 'Narrow'),
+    my_dict['gill_size'] = col2.selectbox('Gill Size', options=('Broad', 'Narrow'),
                    index=None, placeholder='Choose an option')
-    col3.selectbox('Gill Color', options=('Black', 'Brown', 'Buff', 'Chocolate',
+    my_dict['gill_color'] = col3.selectbox('Gill Color', options=('Black', 'Brown', 'Buff', 'Chocolate',
                                           'Gray', 'Green', 'Orange', 'Pink', 'Purple', 'Red', 'White', 'Yellow'), index=None, placeholder='Choose an option')
-    col1.selectbox('Stalk Shape', options=('Enlarging', 'Tapering'),
+    my_dict['stalk_shape'] = col1.selectbox('Stalk Shape', options=('Enlarging', 'Tapering'),
                    index=None, placeholder='Choose an option')
-    col2.selectbox('Stalk Root', options=(
-        'Bulbous', 'Club', 'Cup', 'Equal', 'rhizomorphs'), index=None, placeholder='Choose an option')
-    col3.selectbox('Stalk Serface Above Ring', options=(
+    my_dict['stalk_root'] = col2.selectbox('Stalk Root', options=(
+        'Bulbous', 'Club', 'Cup', 'Equal', 'Rhizomorphs'), index=None, placeholder='Choose an option')
+    my_dict['stalk_surface_above_ring'] = col3.selectbox('Stalk Serface Above Ring', options=(
         'Fibrous', 'Scaly', 'Silky', 'Smooth'), index=None, placeholder='Choose an option')
-    col1.selectbox('Stalk Serface Below Ring', options=(
+    my_dict['stalk_surface_below_ring'] = col1.selectbox('Stalk Serface Below Ring', options=(
         'Fibrous', 'Scaly', 'Silky', 'Smooth'), index=None, placeholder='Choose an option')
-    col2.selectbox('Stalk Color Above Ring', options=(
+    my_dict['Stalk_color_above_ring'] = col2.selectbox('Stalk Color Above Ring', options=(
         'Brown', 'Buff', 'Cinnamon', 'Gray', 'Orange', 'Pink', 'Red', 'White', 'Yellow'), index=None, placeholder='Choose an option')
-    col3.selectbox('Stalk Color Below Ring', options=(
+    my_dict['stalk_color_below_ring'] = col3.selectbox('Stalk Color Below Ring', options=(
         'Brown', 'Buff', 'Cinnamon', 'Gray', 'Orange', 'Pink', 'Red', 'White', 'Yellow'), index=None, placeholder='Choose an option')
-    col1.selectbox('Veil Type', options=('Partial', 'Universal'),
+    my_dict['veil_type'] = col1.selectbox('Veil Type', options=('Partial', 'Universal'),
                    index=None, placeholder='Choose an option')
-    col2.selectbox('Veil Color', options=(
+    my_dict['veil_color'] = col2.selectbox('Veil Color', options=(
         'Brown', 'Orange', 'White', 'Yellow'), index=None, placeholder='Choose an option')
-    col3.selectbox('Ring Number', options=('None', 'One', 'Two'),
+    my_dict['ring_number'] = col3.selectbox('Ring Number', options=('None', 'One', 'Two'),
                    index=None, placeholder='Choose an option')
-    col1.selectbox('Ring Type', options=('Cobwebby', 'Evanescent',
+    my_dict['ring_type'] = col1.selectbox('Ring Type', options=('Cobwebby', 'Evanescent',
                    'Flaring', 'Large', 'None', 'Pendant', 'Sheathing', 'Zone'), index=None, placeholder='Choose an option')
-    col2.selectbox('Spore Print Color', options=('Black', 'Brown', 'Buff',
+    my_dict['spore_print_color'] = col2.selectbox('Spore Print Color', options=('Black', 'Brown', 'Buff',
                    'Chocolate', 'Gray', 'Orange', 'Purple', 'White', 'Yellow'), index=None, placeholder='Choose an option')
-    col3.selectbox('Population', options=('Abundant', 'Clustered',
+    my_dict['population'] = col3.selectbox('Population', options=('Abundant', 'Clustered',
                    'Numerous', 'Scattered', 'Several', 'Solitary'), index=None, placeholder='Choose an option')
-    col1.selectbox('Habitat', options=('Grasses', 'Leaves',
-                   'Meadows', 'Paths', 'urban', 'Waste', 'Woods'), index=None, placeholder='Choose an option')
+    my_dict['habitat'] = col1.selectbox('Habitat', options=('Grasses', 'Leaves',
+                   'Meadows', 'Paths', 'Urban', 'Waste', 'Woods'), index=None, placeholder='Choose an option')
 
-    st.form_submit_button("Submit")
+    submit = st.form_submit_button("Submit")
+
+    url = ''
+
+    if submit:
+        if not all(my_dict.values()):
+            st.error('##### **Please fill all the fields.**')
+        else:
+            req = requests.post(url, data=my_dict)
+
+
+
+
+
